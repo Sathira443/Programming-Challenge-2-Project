bbac34e4fcdf4ea867674ad89bef8a4b854a654b
SahanDC <sahancaldera3@gmail.com>
2022-06-06 12:57:30 +0530

    New Python files, Masks Added

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import cv2
+import numpy as np
 from tracker import *
+from object_detection import ObjectDetection
+
+obj_detection = ObjectDetection()
 
 # Create tracker object
 tracker = EuclideanDistTracker()
 
-cap = cv2.VideoCapture("highway.mp4")
+cap = cv2.VideoCapture("Temp.mp4")
 
 # Object detection from Stable camera
 object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
@@ -14,7 +18,10 @@ while True:
     height, width, _ = frame.shape
 
     # Extract Region of interest
-    roi = frame[340: 720,500: 800]
+    roi = frame[500: 800, 10: 1900]
+    # roi = np.array([[10, 1000], [10, 800], [450, 500], [1500, 500], [1900, 800], [1900, 1000]], np.int32)
+    # region_of_interest = cv2.fillPoly(np.zeros_like(cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY)), roi, 255)
+    # region_of_interest_image = cv2.bitwise_and(cv2.cvtColor(cap, cv2.COLOR_BGR2GRAY), region_of_interest)
 
     # 1. Object Detection
     mask = object_detector.apply(roi)
@@ -26,8 +33,10 @@ while True:
         area = cv2.contourArea(cnt)
         if area > 100:
             #cv2.drawContours(roi, [cnt], -1, (0, 255, 0), 2)
-            x, y, w, h = cv2.boundingRect(cnt)
-
+            # x, y, w, h = cv2.boundingRect(cnt)
+            (class_ids, scores, boxes) = obj_detection.detect(frame)
+            for box in boxes:
+                x, y, w, h = box
 
             detections.append([x, y, w, h])
 
@@ -41,8 +50,9 @@ while True:
     cv2.imshow("roi", roi)
     cv2.imshow("Frame", frame)
     cv2.imshow("Mask", mask)
+    # cv2.imshow("New", region_of_interest_image)
 
-    key = cv2.waitKey(30)
+    key = cv2.waitKey(1)
     if key == 27:
         break
 
